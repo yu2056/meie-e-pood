b308b3171cb071d7a939e4f7d72b06a3e37f5fa3
yu2056 <2056.yuri.sklyarenko.mail.ru@gmail.com>
2025-04-24 15:45:51 +0300

    Next update
    Is problems solved?

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="est">
+    <Html lang="rus">
       <Head />
       <body>
         <Main />
