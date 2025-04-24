a8f52554b6b7f27cba9162139253066b3464032e
yu2056 <2056.yuri.sklyarenko.mail.ru@gmail.com>
2025-04-24 16:09:15 +0300

    Undo Tailwind
    Undo what not working.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-/*import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**//**.{js,ts,jsx,tsx,mdx}",
-    "./components/**//**.{js,ts,jsx,tsx,mdx}",
-    "./app/**//*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -17,4 +17,4 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;*/
+export default config;
